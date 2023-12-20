30ac068d5633c43f3bd0cfca4b2fc9bc9e4cafb5
Abdur <abdur@aequalisys.com>
2023-12-20 16:53:38 +0530

    Initial commit

---
+++ app/ui/src/components/Dashboard/DashboardNewBtn.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+export const DashboardNewBtn = () => {
+  return (
+    <div className="mb-3">
+      <div className="-ml-4 -mt-2 flex flex-wrap items-center justify-end sm:flex-nowrap">
+        <div className="ml-4 mt-2 flex-shrink-0">
+          <Link
+            to="/new"
+            className="cursor-pointer relative inline-flex items-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+          >
+            Create new bot
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
